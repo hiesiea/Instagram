2f8d64bbbaaec73d84f0c2ae423358b010018c95
hiesiea <hiesiea@gmail.com>
2019-08-18 19:07:35 +0900

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Hitoshi KAMADA on 2019/08/18.
+//  Copyright © 2019 hitoshi.kamada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
