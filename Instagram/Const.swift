6e3597bba55e14231e659fa0150e469759428f87
hiesiea <hiesiea@gmail.com>
2019-08-24 19:33:38 +0900

    タイムライン機能まで実装。

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by Hitoshi KAMADA on 2019/08/22.
+//  Copyright © 2019 hitoshi.kamada. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+    static let PostPath = "posts"
+}
